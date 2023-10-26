412da933a89c3d2733420d93f03748accd9acd3a
rahmeh <momanirahmeh53@gmail.com>
2023-10-26 17:55:30 +0300

    first commit

---
+++ app/src/main/java/com/big0soft/codeskiils/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.big0soft.codeskiils.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.big0soft.codeskiils.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
